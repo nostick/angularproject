8666d65197bb361353544051b6ea9fb81261d9ef
Cesar Sulbaran <csulbaran318@gmail.com>
2016-02-24 18:12:07 +0000

    Ionic

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -50,4 +50,39 @@
         </div><!-- /.col -->
 
     </div><!-- /.row -->
+
+    <!-- small box -->
+    <div class="row">
+    <div class="col-md-3">
+    <div class="small-box bg-green">
+        <div class="inner">
+            <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+            <p>Bounce Rate</p>
+        </div>
+        <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+        </div>
+        <a href="#" class="small-box-footer">
+            More info <i class="fa fa-arrow-circle-right"></i>
+        </a>
+    </div>
+    </div>
+        <div class="col-md-3">
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>45<sup style="font-size: 20px">%</sup></h3>
+
+                    <p>Joy Rate</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-heart"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
 @endsection
