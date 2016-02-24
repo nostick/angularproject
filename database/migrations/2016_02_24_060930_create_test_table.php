a816cad9cb4ca9d87e23dcfb0b76bf6cbedb98ed
Cesar Sulbaran <csulbaran318@gmail.com>
2016-02-24 07:10:54 +0000

    Adding new files

---
+++ database/migrations/2016_02_24_060930_create_test_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade')->nullable();
+            $table->string('product')->nullable();
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tests');
+    }
+}
